b7e6388204ce65b84f9e60b4539d751e32d77f90
lakshmikumari-123 <lak1122004@gmail.com>
2026-08-17 15:09:27 +0530

    updated

--- src/main/java/com/devops/ci/Calc/Calculator.java
+++ src/main/java/com/devops/ci/Calc/Calculator.java
@@ -2,6 +2,10 @@ package com.devops.ci.Calc;
 
 public class Calculator 
 {
+	public static void main(String[] args)
+	{
+		System.out.println("Calculator started");
+	}
 	public int add(int a,int b)
 	{
 		return a+b;
